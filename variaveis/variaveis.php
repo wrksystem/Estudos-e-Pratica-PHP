9b3fcf93ff4e5dd7fc891791a45f108265447b3f
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2022-03-31 21:55:38 -0300

    Update variaveis.php
    Explicação sobre camelcase.

--- variaveis/variaveis.php
+++ variaveis/variaveis.php
@@ -1,5 +1,10 @@
 <?php
 
 $nome = "Hcode";
+//seguindo as boas práticas e o método de camelcase para variáveis.
+//varível de nome simples, apenas letras minúsculas.
+
+$segundoNome = "CursosUdemy";
+//variável de nome composto, segunda palavra começa com letra maiúscula.
 
 ?>
