6a460a2156c891d0e99561b46ab6c2bb45abf6f5
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2022-03-31 22:25:43 -0300

    Create tiposdados.php

---
+++ tiposdados.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "Hcode";//string
+$site = 'www.code.com.br';//string
+
+$ano = 1990;//inteiro
+$salario = 5500.99;//ponto flutuante
+$bloqueado = false;//boleano
+
+////Esses são os tipos básicos////
+
+$frutas = array("abacaxi", "laranja", "manga");//array
+$nascimento = new DateTime();//objeto
+
+
+//$arquivo = fopen("tiposdados.php", "r");
+//var_dump($nascimento);//verifica que é um objeto
+//echo $frutas[2];//esse estaria puxando a posição 2 do array sendo que esse começa em 0.
+
+
+?>
