2bd13d1ff412f9fbe2c7d2b4a9b23bbd77b9a7b7
Werik Rodrigues <51803873+wrksystem@users.noreply.github.com>
2022-04-03 22:31:20 -0300

    Create operadoraritimetico.php

---
+++ operadores/operadoraritimetico.php
@@ -0,0 +1,22 @@
+<?php
+//esse são todos os operadores aritiméticos 
+
+$a = 10;
+$b = 2;
+
+echo $a + $b;
+echo "<br>";
+
+echo $a - $b;
+echo "<br>";
+
+echo $a * $b;
+echo "<br>";
+
+echo $a / $b;
+echo "<br>";
+
+echo $a % $b;
+echo "<br>";
+
+?>
